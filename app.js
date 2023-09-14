36368d89680dac222d66ca295c8e71755bd0700f
syedwaleedali20 <syedwaleed887@gmail.com>
2023-09-14 16:29:40 -0700

    now ready to commit

---
+++ app.js
@@ -0,0 +1,127 @@
+// var weight = +prompt("Enter your weight");
+// var age = +prompt("Enter your Age");
+// var gender = prompt("Enter your gender");
+
+// if ( gender === "male" || age >= 20 && weight >= 75){
+//     console.log("com to our try out..");
+// } else{
+//     console.log("sorry..");
+// }
+
+// var lang = prompt("Enter your laguage");
+// var location = prompt("Enter your place");
+// var exp = prompt("Enter your experience");
+
+// if(lang === "javascript")
+
+// Q1
+// var student = [];
+// Q2
+// Q3
+// var hello = [""];
+// var world = [""];
+// Q4
+// var num1 = [];
+// var num2 = [];
+// // Q5
+// var num1 = [];
+// var num1 = [];
+
+// Q6
+// var arr = ["hello sir","waleed","Aaraiz","abc"];
+// console.log(arr);
+
+// // Q7
+// var qualifications = [];
+// qualifications[0] = "<b><i>1)Qualifications ";
+// qualifications[1] = "<br> <br>2)SSC";
+// qualifications[2] = "<br>3)HSC";
+// qualifications[3] = "<br>4)BSC";
+// qualifications[4] = "<br>5)BS";
+// qualifications[5] = "<br>6)BCOM";
+// qualifications[6] = "<br>7)MS";
+// qualifications[7] = "<br>8)M.PHIL";
+// qualifications[8] = "<br>9)PHD";
+
+// document.write(qualifications)
+
+// Q8
+// var arr = [320, 450, 550];
+// var arr = [320, 450, 550];
+// var arr = [320, 450, 550];
+// var total = [500]
+// var arr1 = [arr* 100 / total]
+
+
+// document.write("score of michael is" + arr[0] + "percentage is<br>"+ arr1);
+// document.write("score of John is" + arr[1] + "percentage is<br>" + arr1);
+// document.write("score of Tony is" + arr[2] + "percentage is<br>" + arr1);
+//Q9..
+// //10
+// let studentScores = [320, 230, 480, 120];
+
+// // Print the unsorted scores
+// console.log("Unsorted scores:", studentScores);
+
+// // Sort the scores in ascending order
+// studentScores.sort((a, b) => a - b);
+
+// // Print the sorted scores
+// console.log("Sorted scores:", studentScores);
+//Q11
+// // Initialize the array with city names
+// let cities = ['Karachi', 'Lahore', 'Islamabad', 'Quetta', 'Peshawar'];
+
+// // Initialize the selectedCities array and copy three elements from cities
+// let selectedCities = cities.slice(2, 5); // This copies elements at indices 2, 3, and 4
+
+// // Print the lists
+// console.log("City list:", cities);
+// console.log("Selected cities list:", selectedCities);
+
+// //Q12
+// var arr = ["This ", " is ", " my ", " cat"];
+
+
+// var singleString = arr.join("");
+
+// console.log("Single string:", singleString);
+
+//Q13
+// let devices = [];
+// devices.push("keyboard");
+// devices.push("mouse");
+// devices.push("printer");
+// devices.push("monitor");
+
+// // Access values in FIFO order
+// console.log("out:", devices.shift());
+// console.log("out:", devices.shift());
+// console.log("out:", devices.shift());
+// console.log("out:", devices.shift());
+
+//Q14
+// let devices = [];
+
+
+// devices.push("keyboard");
+// devices.push("mouse");
+// devices.push("printer");
+// devices.push("monitor");
+
+
+// console.log("out:", devices.pop());
+// console.log("out:", devices.pop());
+// console.log("out:", devices.pop());
+// console.log("out:", devices.pop());
+
+//Q15
+// const phoneManufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+
+// document.write("<select>");
+
+// for (let i = 0; i < phoneManufacturers.length; i++) {
+//     document.write("<option value='" + phoneManufacturers[i] + "'>" + phoneManufacturers[i] + "</option>");
+// }
+
+// document.write("</select>");
